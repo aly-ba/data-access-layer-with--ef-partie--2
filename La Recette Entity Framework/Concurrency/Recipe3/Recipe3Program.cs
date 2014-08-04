04ed0b148d0963aff228f4e00d3c46d69c93596c
aly ba <alybas2002@yahoo.fr>
2014-08-04 12:51:04 +0200

    recipes EF6

---
+++ La Recette Entity Framework/Concurrency/Recipe3/Recipe3Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Apress.EF6Recipes.Concurrency.Recipe3
+{
+    public static class Recipe3Program
+    {
+        public static void Run()
+        {
+            using (var context = new Recipe3Context())
+            {
+                using (var scope1 = new TransactionScope())
+                {
+                    // save, but don't commit
+                    var outerEmp = new Employee { Name = "Karen Stanfield" };
+                    Console.WriteLine("Outer employee: {0}", outerEmp.Name);
+                    context.Employees.Add(outerEmp);
+                    context.SaveChanges();
+
+                    // second transaction for read uncommitted
+                    using (var innerContext = new Recipe3Context())
+                    {
+                        using (var scope2 = new TransactionScope(
+                            TransactionScopeOption.RequiresNew,
+                            new TransactionOptions
+                            {
+                                IsolationLevel = IsolationLevel.ReadUncommitted
+                            }))
+                        {
+                            var innerEmp = innerContext.Employees
+                                            .First(e => e.Name == "Karen Stanfield");
+                            Console.WriteLine("Inner employee: {0}", innerEmp.Name);
+                            scope1.Complete();
+                            scope2.Complete();
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ La Recette Entity Framework/Concurrency/Recipe6/Recipe6Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apress.EF6Recipes.Concurrency.Recipe6
+{
+    public static class Recipe6Program
+    {
+        public static void Run()
+        {
+            using (var context = new Recipe6Context())
+            {
+                var student = new Student
+                {
+                    Name = "Joan Williams",
+                    EnrollmentDate = DateTime.Parse("1/12/2010")
+                };
+                var instructor = new Instructor
+                {
+                    Name = "Rodger Keller",
+                    HireDate = DateTime.Parse("7/14/1992")
+                };
+                context.People.Add(student);
+                context.People.Add(instructor);
+                context.SaveChanges();
+            }
+
+            using (var context = new Recipe6Context())
+            {
+                // find the student and update the enrollment date
+                var student = context.People.OfType<Student>()
+                                .First(s => s.Name == "Joan Williams");
+                Console.WriteLine("Updating {0}'s enrollment date", student.Name);
+
+                // out-of-band update occurs
+                Console.WriteLine("[Apply rogue update]");
+                context.Database.ExecuteSqlCommand(@"update chapter14.person set name = 'Joan Smith'
+          where personId = 
+          (select personId from chapter14.person where name = 'Joan Williams')");
+
+                // change the enrollment date
+                student.EnrollmentDate = DateTime.Parse("5/2/2010");
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    Console.WriteLine("Exception: {0}", ex.Message);
+                }
+            }
+            
+        }
+    }
+}
